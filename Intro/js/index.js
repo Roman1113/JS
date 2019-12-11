cc3ecbd5d62d1c547ba633a9f9591daf381c787c
sasha_tkachuk <atkachuk@radiantrfid.com>
2019-12-11 11:56:02 +0200

    first commit

---
+++ Intro/js/index.js
@@ -0,0 +1,71 @@
+console.log("Hello world!!!");
+
+// var a = 5; //ES 5
+// let b = 10; //ES 6
+// var c = "Vasia";
+
+// console.log("B =>", b);
+// console.log("C =>", c);
+// console.log("C =>", c, "type =>", typeof (c));
+// c = 10000;
+// console.log("C after", c);
+// document.write("<h2>", b + a, "</h2>");
+
+// console.log("C after", c, "type =>", typeof (c));
+
+// let name = prompt("Enter name:");
+// document.write(name, "type =>", typeof(name));
+
+// let name2 = parseInt(prompt("Enter name:"));
+// document.write(name, "type =>", typeof(name2));
+
+// let a = parseInt(prompt("Enter first"));
+// let b = parseInt(prompt("Enter second"));
+
+// if (a < b) {
+//     document.write(a, ">", b);
+// } else if (a < b) {
+//     document.write(a, "<", b);
+// } else {
+//     document.write(a, "=", b);
+// }
+
+// let c = parseInt(prompt("Enter first"));
+// let d = parseInt(prompt("Enter second"));
+
+// if (c == d) {
+//     document.write("true");
+// } else {
+//     document.write("false");
+// }
+
+// let day = prompt("Enter days");
+
+// switch (day) {
+//     case '1': document.write("manday");
+//         break;
+//     default: document.write("RTFM");
+//         break;
+// }
+
+// const arr=[10,"hhh",50,[10,"ooo",[90,"ttt"]]];
+// console.log(arr);
+// document.write(arr);
+let text = "Відомо, що 1 дюйм дорівнює 2.54 см. " +
+    "Розробити програму, що переводить дюйми в сантиметри и навпаки. "
+document.write(text);
+
+let d = 2.54;
+let cm = 0;
+document.write("Enter 1 if (дюйми в сантиметри)");
+document.write("Enter 2 if (сантиметри в дюйми)");
+// switch (res) {
+//     case '1': let a = prompt("Enter cm");
+//     a=cm*2.54;
+//     document.write(a);
+//     break;
+//     case '2': let  b= prompt("Enter d");
+//     b=d/2.54;
+//     document.write(b);
+//     break;
+// }
