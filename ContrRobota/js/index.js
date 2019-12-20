6848dcdf59a94bdd6814ed68a53617713783b337
maksutroman <maksutroman@gmail.com>
2019-12-20 14:41:34 +0200

    add some text

--- ContrRobota/js/index.js
+++ ContrRobota/js/index.js
@@ -1,7 +1,8 @@
-//window.addEventListener("load", Init);
-
-const btn_send = document.querySelector(".btn_send");
-btn_send.addEventListener("click", Init);
+window.addEventListener("load", Start);
+function Start() {
+    const btn_send = document.querySelector(".btn_send");
+    btn_send.addEventListener("click", Init);
+}
 
 function Init() {
     const text = document.querySelector(".text");
@@ -9,10 +10,11 @@ function Init() {
     const URL = `https://api.privatbank.ua/p24api/infrastructure?json&tso&address=&city=${city}`;
     let printCity = document.querySelector(".locations");
     let locations = document.getElementsByClassName(".locations");
-    if (locations.length > 0) {
+    if (locations.length > 1) {
         printCity.removeChild(printCity.lastChild);
     }
     Request(URL);
+    initMap();
 }
 
 function Request(URL) {
@@ -30,7 +32,11 @@ function Request(URL) {
                 table.setAttribute("width", "300px");
                 for (let i = 0; i < data.devices.length; i++) {
                     let div = document.createElement("tr");
-                    div.innerHTML = `<td>${[i + 1]}</td><td>${data.devices[i].fullAddressUa}</td><td>${data.devices[i].placeUa}</td><td>${data.devices[i].cityUA}</td>`;
+                    div.innerHTML = `<td>${[i + 1]}.</td>
+                    <td>${data.devices[i].fullAddressUa}</td>
+                    <td>${data.devices[i].placeUa}</td>
+                    <td>${data.devices[i].cityUA}</td>
+                    <button id = but_${[i + 1]} style = "margin-left = 10px">on map</button>`;
                     table.appendChild(div);
                 }
                 local_Info.appendChild(table);
@@ -43,12 +49,19 @@ function Request(URL) {
     xhr.send();
 }
 
+function clickBtn() {
+    var button = document.getElementById(btn_1);
+    button.addEventListener("click", button)
+
+}
+
+
 function initMap() {
     // The location of Uluru
-    var uluru = { lat: -25.344, lng: 131.036 };
+    var uluru = { lat: 50.616810, lng: 26.273060 };
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: uluru });
+        document.getElementById('map'), { zoom: 15, center: uluru });
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({ position: uluru, map: map });
 }
